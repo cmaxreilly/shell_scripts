162277969b7c4d9e29aaf3dd60353210bf693670
cmaxreilly <c.maxwell.reilly@gmail.com>
2023-10-18 11:54:55 -0600

    Added some useful scripts

---
+++ init_day.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+open -a Safari "https://calendar.google.com/calendar/u/0/r" "https://mail.google.com/mail/u/0/#inbox"
+
+open "https://outlook.office.com/mail/inbox/id/AAQkADY4OTM1OTFiLWVjZGMtNDJmNS05ZDlkLTBiZmEwYzMwNzIxYwAQAJ0MildW9SBIjZE7zn54P50%3D"
+# open -n /Applications/Safari.app "https://mail.google.com/mail/u/0/#inbox"
+# open /Applications/Safari.app "https://outlook.office.com/mail/inbox/id/AAQkADY4OTM1OTFiLWVjZGMtNDJmNS05ZDlkLTBiZmEwYzMwNzIxYwAQAJ0MildW9SBIjZE7zn54P50%3D"
